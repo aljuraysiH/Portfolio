81dbc6431e4b36bf354dbe8f080843223c2060a7
Hamad Aljuraysi <AljuraysiH@gmail.com>
2022-09-23 15:36:05 +0300

    add env file

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import { useEffect, useState } from "react";
-import Navbar from "./components/Navbar/Navbar";
-import { Route, Routes, useLocation } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
-import { useTranslation } from "react-i18next";
-import Home from "./Pages/Home/Home";
-import Projects from "./Pages/Projects/Projects";
-import Modal from "./components/Modal/Modal";
-import { ToastContainer } from "react-toastify";
+import { useEffect, useState } from 'react';
+import Navbar from './components/Navbar/Navbar';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+import { useTranslation } from 'react-i18next';
+import Home from './Pages/Home/Home';
+import Projects from './Pages/Projects/Projects';
+import Modal from './components/Modal/Modal';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   const { i18n } = useTranslation();
   const location = useLocation();
   const [language, setLanguage] = useState(() => {
-    let lang = "en";
-    if (localStorage.getItem("language")) {
-      lang = localStorage.getItem("language");
+    let lang = 'en';
+    if (localStorage.getItem('language')) {
+      lang = localStorage.getItem('language');
     }
-    return lang ?? "";
+    return lang ?? '';
   });
 
   useEffect(() => {
-    localStorage.setItem("language", language);
+    localStorage.setItem('language', language);
   }, [language]);
 
   useEffect(() => {
     i18n.changeLanguage(language);
-    if (i18n.language === "ar") {
-      document.body.dir = "rtl";
+    if (i18n.language === 'ar') {
+      document.body.dir = 'rtl';
     } else {
-      document.body.dir = "ltr";
+      document.body.dir = 'ltr';
     }
   }, [language, i18n]);
 
@@ -36,25 +36,25 @@ function App() {
     <>
       <Modal />
       <ToastContainer
-        position="top-center"
+        position='top-center'
         autoClose={3000}
         closeOnClick
-        rtl={i18n.language === "ar" ? true : false}
+        rtl={i18n.language === 'ar' ? true : false}
         pauseOnFocusLoss
         draggable
         pauseOnHover
       />
 
-      <div className={`app ${i18n.language === "ar" ? "ar" : ""}`}>
+      <div className={`app ${i18n.language === 'ar' ? 'ar' : ''}`}>
         <AnimatePresence exitBeforeEnter>
           <Routes location={location} key={location.key}>
             <Route
-              path="/"
+              path='/'
               element={<Navbar language={language} setLanguage={setLanguage} />}
             >
               <Route index element={<Home />} />
-              <Route path="*" element={<Home />} />
-              <Route path="/projects" element={<Projects />} />
+              <Route path='*' element={<Home />} />
+              <Route path='/projects' element={<Projects />} />
             </Route>
           </Routes>
         </AnimatePresence>
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,11 +1,11 @@
-import styles from "./Modal.module.scss";
-import emailjs from "@emailjs/browser";
-import { useRef, useState } from "react";
-import useModal from "../../hooks/useModal";
-import { MdEmail, MdClose } from "react-icons/md";
-import { useTranslation } from "react-i18next";
-import { toast } from "react-toastify";
-import Loader from "../Loader/Loader";
+import styles from './Modal.module.scss';
+import emailjs from '@emailjs/browser';
+import { useRef, useState } from 'react';
+import useModal from '../../hooks/useModal';
+import { MdEmail, MdClose } from 'react-icons/md';
+import { useTranslation } from 'react-i18next';
+import { toast } from 'react-toastify';
+import Loader from '../Loader/Loader';
 
 function Modal() {
   const { showModal, setShowModal } = useModal();
@@ -18,21 +18,21 @@ function Modal() {
     setisLoading(true);
     emailjs
       .sendForm(
-        "service_w9y2ixv",
-        "template_7d22jxf",
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
         form.current,
-        "2YQQOIFrLrRwRB5g9"
+        '2YQQOIFrLrRwRB5g9'
       )
       .then(
         () => {
           setisLoading(false);
-          toast.success(t("notification"));
+          toast.success(t('notification'));
           e.target.reset();
           setShowModal(!showModal);
         },
         (error) => {
           setisLoading(false);
-          toast.error("Something went wrong");
+          toast.error('Something went wrong');
           console.log(error.text);
         }
       );
@@ -41,7 +41,7 @@ function Modal() {
   return (
     <>
       {isLoading && <Loader isLoading={isLoading} />}
-      <div className={`${styles.container} ${showModal ? styles.active : ""}`}>
+      <div className={`${styles.container} ${showModal ? styles.active : ''}`}>
         <div
           onClick={() => setShowModal(!showModal)}
           className={styles.bg}
@@ -51,25 +51,25 @@ function Modal() {
             className={styles.close}
             onClick={() => setShowModal(!showModal)}
           />
-          <h2>{t("contact")}</h2>
-          <div className={styles["modal-container"]}>
-            <div className={styles["icon-container"]}>
+          <h2>{t('contact')}</h2>
+          <div className={styles['modal-container']}>
+            <div className={styles['icon-container']}>
               <MdEmail className={styles.icon} />
             </div>
             <form ref={form} onSubmit={sendEmail}>
               <div>
-                <label htmlFor="name">{t("name_form")}</label>
-                <input id="name" type="text" name="user_name" required />
+                <label htmlFor='name'>{t('name_form')}</label>
+                <input id='name' type='text' name='user_name' required />
               </div>
               <div>
-                <label htmlFor="email">{t("email")}</label>
-                <input id="email" type="email" name="user_email" required />
+                <label htmlFor='email'>{t('email')}</label>
+                <input id='email' type='email' name='user_email' required />
               </div>
               <div>
-                <label htmlFor="message">{t("message")}</label>
-                <textarea id="message" name="message" required />
+                <label htmlFor='message'>{t('message')}</label>
+                <textarea id='message' name='message' required />
               </div>
-              <button type="submit">{t("send")}</button>
+              <button type='submit'>{t('send')}</button>
             </form>
           </div>
         </div>
